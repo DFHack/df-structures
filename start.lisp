887adc8e726f841f7e9239706ab7b6100dd50ea5
Alexander Gavrilov <angavrilov@gmail.com>
2011-11-27 16:47:28 +0400

    Guess more data on legends.

--- start.lisp
+++ start.lisp
@@ -8,7 +8,8 @@
         :cl-linux-debug.field-names
         :cl-linux-debug.data-defs
         :cl-linux-debug.data-info
-        :cl-linux-debug.gui))
+        :cl-linux-debug.gui)
+  (:import-from :cl-linux-debug.code-info #:parse-int))
 
 (in-package :work)
 
@@ -27,3 +28,21 @@
 
 (defun browse (obj)
   (browse-object-in-new-window *memory* obj))
+
+(reload)
+(refresh)
+
+
+(defun ctor-addresses ()
+  (let ((section (find-section-by-name (main-image-of (executable-of *process*)) ".ctors")))
+    (loop for i from 0 below (length-of section) by 4
+       for val = (parse-int (data-bytes-of section) i 4)
+       unless (member val '(0 #xFFFFFFFF))
+       collect val)))
+
+(defun disasm-at-addr (address size)
+  (with-bytes-for-ref (vector offset *memory* size address)
+    (dolist (cmd (disassemble-all vector :start offset :end (+ offset size)
+                                  :base-address address :errorp nil))
+      (when cmd
+        (format t "~10X: ~A~%" (x86-instruction-address cmd) (x86-instruction-text cmd))))))
