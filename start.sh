0e2b951f34231f98769cfaaf2f66226b0db2942a
Alexander Gavrilov <angavrilov@gmail.com>
2012-01-08 16:59:24 +0400

    Tweak the start.sh script.

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 ./sbcl-runtime --dynamic-space-size 1500 \
     --no-sysinit --no-userinit \
     --eval '(pushnew "cl-linux-debug/" asdf:*central-registry*)' \
-    --load start.lisp --eval '(in-package :work)'
+    --load start.lisp --eval '(in-package :work)' \
+    --eval '(browse $global.*)'
