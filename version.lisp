8ee67c10aefd17395ad0d7a99eb65fbbdbc297d2
Alexander Gavrilov <angavrilov@gmail.com>
2012-03-23 14:58:52 +0400

    Reset for v0.34.06.

--- version.lisp
+++ version.lisp
@@ -1,3 +1,3 @@
-(defparameter *df-version-str* "v0.34.05")
-(defparameter *windows-timestamp* #x4F55F23E)
-(defparameter *linux-hash* "b53b4cfc6587023eda5322bb28011b1c")
+(defparameter *df-version-str* "v0.34.06")
+(defparameter *windows-timestamp* #x4f6c34ca)
+(defparameter *linux-hash* "304f8e89577ac1ab733d71a743576d1e")
