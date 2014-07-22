0086747b90ee121eb82ee8442edb1393fdce1ac4
Alexander Gavrilov <angavrilov@gmail.com>
2014-07-22 17:50:22 +0400

    Update the current version to 0.40.04

--- version.lisp
+++ version.lisp
@@ -1,4 +1,4 @@
-(defparameter *df-version-str* "v0.40.01")
-(defparameter *windows-timestamp* #x53bb189a)
-(defparameter *linux-hash* "d3460da2da1e5da23a4c4db4484b4418")
-(defparameter *osx-hash* "db93fe97b4d46cf4f79b3f6ebc2af70a")
+(defparameter *df-version-str* "v0.40.04")
+(defparameter *windows-timestamp* #x53cc0058)
+(defparameter *linux-hash* "9fdc4a78cebf01ba40a7c377531dba0d")
+(defparameter *osx-hash* "06154bf5af4855023f5acfabca2f7411")
