a94612025c3c9f8d9075f748c839782220e5a460
Alexander Gavrilov <angavrilov@gmail.com>
2014-08-25 00:33:07 +0400

    Bump version up to v0.40.10

--- version.lisp
+++ version.lisp
@@ -1,4 +1,4 @@
-(defparameter *df-version-str* "v0.40.09")
-(defparameter *windows-timestamp* #x53f0f350)
-(defparameter *linux-hash* "9e592efe3fb8a3467ae25f0d41d423e1")
-(defparameter *osx-hash* "a17cd1fc729612aeab32d4dacba67438")
+(defparameter *df-version-str* "v0.40.10")
+(defparameter *windows-timestamp* #x53fa3213)
+(defparameter *linux-hash* "0b7185da37ac3a729f0972c9aff512ca")
+(defparameter *osx-hash* "89fa6d002a9dd1cb08590cc95d0974c1")
