4f75103e959f3af71ab3da67a360a2c5859e1dd5
Alexander Gavrilov <angavrilov@gmail.com>
2014-11-06 15:30:29 +0300

    Update version to v0.40.15

--- version.lisp
+++ version.lisp
@@ -1,4 +1,4 @@
-(defparameter *df-version-str* "v0.40.14")
-(defparameter *windows-timestamp* #x544bffa4)
-(defparameter *linux-hash* "8ab84807a405a7fa697847e6b3ecb00f")
-(defparameter *osx-hash* "12b458920f49376cbaced0549a092f2e")
+(defparameter *df-version-str* "v0.40.15")
+(defparameter *windows-timestamp* #x545a6968)
+(defparameter *linux-hash* "f1049a2b4a50246c233a465a67ebe96e")
+(defparameter *osx-hash* "9aa7bc3c1e745b98ab4d2b3763b8d0e7")
