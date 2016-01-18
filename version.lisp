acc27f313b63a5d86364b34a25ad70b94c2efdff
lethosor <lethosor@gmail.com>
2016-01-18 10:29:33 -0500

    Run initial scripts for 0.42.05

--- version.lisp
+++ version.lisp
@@ -1,4 +1,4 @@
-(defparameter *df-version-str* "v0.42.04")
-(defparameter *windows-timestamp* #x567ef345)
-(defparameter *linux-hash* "ddd87cd8ebd512790415866bf5de4c4f")
-(defparameter *osx-hash* "be270c5f694d545dd551612f2e195594")
+(defparameter *df-version-str* "v0.42.05")
+(defparameter *windows-timestamp* #x569bf3df)
+(defparameter *linux-hash* "de7a048b54f43431bf5c5d705228a5f8")
+(defparameter *osx-hash* "a3d3bf4838fd39d502a4d559fd453547")
