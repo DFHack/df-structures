5035f75f6ccb80706b663dadfbedc754e9c1da1b
lethosor <lethosor@gmail.com>
2020-12-30 17:57:41 -0500

    Avoid changing working directory

--- tools/generate-type-dump.py
+++ tools/generate-type-dump.py
@@ -4,7 +4,7 @@ import subprocess
 
 import jinja2
 
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+xml_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--template", required=True)
@@ -12,7 +12,12 @@ parser.add_argument("--output", required=True)
 parser.add_argument("--perl", required=True)
 args = parser.parse_args()
 
-types = subprocess.check_output([args.perl, "../list.pl", "..", "df"]).decode().replace("\r", "").split("\n")
+types = subprocess.check_output([
+    args.perl,
+    os.path.join(xml_root, "list.pl"),
+    xml_root,
+    "df",
+]).decode().replace("\r", "").split("\n")
 types = [t.split("/")[-1].split(".")[0] for t in types
     if t.endswith(".h") and "global_objects.h" not in t]
 types.sort()
